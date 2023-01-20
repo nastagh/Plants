1f1a876f77588445c4e534ecb51c5466909cc65a
Anastasiya <nastagh96@mail.ru>
2023-01-20 16:39:37 +0100

    fix: fix paths

--- script.js
+++ script.js
@@ -53,7 +53,7 @@ const projects = [
     id: 2,
     img: {
       srcset:
-        "../../assets/images/first-2.png     1220w, ../../assets/images/first-2-768.png  768w",
+        "./assets/images/first-2.png     1220w, ./assets/images/first-2-768.png  768w",
       sizes: mediaSize,
       src: "../../assets/images/first-2.png",
       alt: "Planting",
@@ -65,7 +65,7 @@ const projects = [
     id: 3,
     img: {
       srcset:
-        "../../assets/images/first-3.png     1220w, ../../assets/images/first-3-768.png  768w",
+        "./assets/images/first-3.png     1220w, ./assets/images/first-3-768.png  768w",
       sizes: mediaSize,
       src: "../../assets/images/first-3.png",
       alt: "Lawn care",
@@ -77,7 +77,7 @@ const projects = [
     id: 4,
     img: {
       srcset:
-        "../../assets/images/first-4.png     1220w, ../../assets/images/first-4-768.png  768w",
+        "./assets/images/first-4.png     1220w, ./assets/images/first-4-768.png  768w",
       sizes: mediaSize,
       src: "../../assets/images/first-4.png",
       alt: "Planting",
@@ -89,7 +89,7 @@ const projects = [
     id: 5,
     img: {
       srcset:
-        "../../assets/images/first-5.png     1220w, ../../assets/images/first-5-768.png  768w",
+        "./assets/images/first-5.png     1220w, ./assets/images/first-5-768.png  768w",
       sizes: mediaSize,
       src: "../../assets/images/first-5.png",
       alt: "Garden care",
@@ -101,7 +101,7 @@ const projects = [
     id: 6,
     img: {
       srcset:
-        "../../assets/images/first-6.png     1220w, ../../assets/images/first-6-768.png  768w",
+        "./assets/images/first-6.png     1220w, ./assets/images/first-6-768.png  768w",
       sizes: mediaSize,
       src: "../../assets/images/first-6.png",
       alt: "Planting",
